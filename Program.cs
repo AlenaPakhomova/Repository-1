3fca8406a0d8987cd8f2c8499b6b7310121c0073
Алёна Пахомова <120586326+AlenaPakhomova@users.noreply.github.com>
2022-12-15 00:18:27 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+
+
+
+using System.IO;
+using System.Text.RegularExpressions;
+
+Console.WriteLine("Введите текст:");
+string text = Console.ReadLine();
+string[] words = text.Split(new[] { '!', '?', ' ' });
+
+Regex name = new Regex(@"www\.[a-z]*\.[a-z]*\.ru");
+
+//Ищет во входной строке все вхождения регулярного выражения и возвращает все соответсвия
+
+MatchCollection mat = name.Matches(text);
+
+Console.WriteLine("\nДоменные имена:");
+
+foreach (Match m in name.Matches(text))
+{
+    Console.WriteLine(m.Value);
+}
+
